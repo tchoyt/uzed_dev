1de404858705516ca65eede24765ce167523f15d
Tim Hoyt <timothy.hoyt@invincealabs.com>
2017-03-08 16:43:32 -0500

    Fix rootFS script

--- scripts/wr_rootpart.sh
+++ scripts/wr_rootpart.sh
@@ -5,7 +5,7 @@ function cp_rootfs()
 {
 	if [ -e ../*-*-*-armhf-*/armhf-rootfs-*.tar ]
 	then
-		tar xfvp ${ROOTFS_TAR_DIR}/*-*-*-armhf-*/armhf-rootfs-*.tar -C ${ROOTFS_INSTALL_DIR}
+		sudo tar xfvp ${ROOTFS_TAR_DIR}/*-*-*-armhf-*/armhf-rootfs-*.tar -C ${ROOTFS_INSTALL_DIR}
 	else
 		echo 'Need to run sw_init.sh to populate rootFS tarball'
 	fi		
@@ -14,19 +14,19 @@ function cp_rootfs()
 # Copy kernel modules
 function cp_modules()
 {
-	cp -rv ${INSTALL_MOD_PATH}/lib ${ROOTFS_INSTALL_DIR}/.
+	sudo cp -rv ${INSTALL_MOD_PATH}/lib ${ROOTFS_INSTALL_DIR}/.
 }
 
 # Copy SoCFPGA applications
 function cp_apps()
 {
-	cp -v ${SOCFPGA_SW_DIR}/axi_gp0_rw ${ROOTFS_INSTALL_DIR}/usr/local/bin/.
+	sudo cp -v ${SOCFPGA_SW_DIR}/axi_gp0_rw ${ROOTFS_INSTALL_DIR}/usr/local/bin/.
 }
 
 # Write /etc/fstab
 function wr_fstab()
 {
-	sh -c "echo '
+	sudo sh -c "echo '
 /dev/mmcblk0p2  /  auto errors=remount-ro  0  1
 /dev/mmcblk0p1  /boot/uboot auto defaults  0  2' >> ${ROOTFS_INSTALL_DIR}/etc/fstab"
 }
@@ -34,7 +34,7 @@ function wr_fstab()
 # Write /etc/network/interfaces
 function wr_ethinterface()
 {
-	sh -c "echo '
+	sudo sh -c "echo '
 auto lo
 iface lo inet loopback
   
@@ -49,14 +49,14 @@ iface eth0 inet static
 # Write /etc/hosts, /etc/hostname
 function wr_hostname()
 {
-	sed -i '/127.0.1.1/c\127.0.1.1\t'"${BOARD_HOSTNAME}"'.localdomain\t'"${BOARD_HOSTNAME}" ${ROOTFS_INSTALL_DIR}/etc/hosts
-	sed -i '/arm/c\'"${BOARD_HOSTNAME}" ${ROOTFS_INSTALL_DIR}/etc/hostname
+	sudo sed -i '/127.0.1.1/c\127.0.1.1\t'"${BOARD_HOSTNAME}"'.localdomain\t'"${BOARD_HOSTNAME}" ${ROOTFS_INSTALL_DIR}/etc/hosts
+	sudo sed -i '/arm/c\'"${BOARD_HOSTNAME}" ${ROOTFS_INSTALL_DIR}/etc/hostname
 }
 
 # Write /etc/ssh/sshd_config
 function wr_ssh_config()
 {
-	sed -i '/PermitRootLogin prohibit-password/c\PermitRootLogin yes' ${ROOTFS_INSTALL_DIR}/etc/ssh/sshd_config
+	sudo sed -i '/PermitRootLogin prohibit-password/c\PermitRootLogin yes' ${ROOTFS_INSTALL_DIR}/etc/ssh/sshd_config
 }
 
 # Define variables
