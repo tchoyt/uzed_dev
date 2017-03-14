25a784917b782ab560618186521309c74eec9384
Tim Hoyt <timothy.hoyt@invincealabs.com>
2017-03-14 11:04:27 -0400

    Updated FPGA build with SDK export

--- fpga/uzed_base_system/uzed_base_system.srcs/sources_1/bd/zynq_top/ip/zynq_top_rst_ps7_0_100M_0/zynq_top_rst_ps7_0_100M_0_stub.v
+++ fpga/uzed_base_system/uzed_base_system.srcs/sources_1/bd/zynq_top/ip/zynq_top_rst_ps7_0_100M_0/zynq_top_rst_ps7_0_100M_0_stub.v
@@ -1,7 +1,7 @@
 // Copyright 1986-2016 Xilinx, Inc. All Rights Reserved.
 // --------------------------------------------------------------------------------
 // Tool Version: Vivado v.2016.4 (lin64) Build 1733598 Wed Dec 14 22:35:42 MST 2016
-// Date        : Fri Feb 17 13:41:58 2017
+// Date        : Tue Mar 14 10:59:19 2017
 // Host        : thoyt-dell7510 running 64-bit Ubuntu 16.04.2 LTS
 // Command     : write_verilog -force -mode synth_stub
 //               /home/thoyt/fpga_work/uzed_dev/fpga/uzed_base_system/uzed_base_system.srcs/sources_1/bd/zynq_top/ip/zynq_top_rst_ps7_0_100M_0/zynq_top_rst_ps7_0_100M_0_stub.v
