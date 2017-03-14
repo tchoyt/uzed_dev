dcaac6ae74acda01ce57b9159a44a3e3fb56631e
Tim Hoyt <timothy.hoyt@invincealabs.com>
2017-03-14 15:11:03 -0400

    Fix /etc/hosts

--- scripts/wr_rootpart.sh
+++ scripts/wr_rootpart.sh
@@ -43,7 +43,14 @@ iface eth0 inet dhcp
 # Write /etc/hosts, /etc/hostname
 function wr_hostname()
 {
-	sudo sh -c "echo '127.0.1.1	${BOARD_HOSTNAME}.localdomain	${BOARD_HOSTNAME}' > ${ROOTFS_INSTALL_DIR}/etc/hosts"
+	sudo sh -c "echo '127.0.0.1	localhost
+127.0.1.1	${BOARD_HOSTNAME}.localdomain	${BOARD_HOSTNAME}
+
+# The following lines are desirable for IPv6 capable hosts
+::1     localhost ip6-localhost ip6-loopback
+ff02::1 ip6-allnodes
+ff02::2 ip6-allrouters' > ${ROOTFS_INSTALL_DIR}/etc/hosts"
+
 	sudo sh -c "echo '${BOARD_HOSTNAME}' > ${ROOTFS_INSTALL_DIR}/etc/hostname"
 }
 
