7efb378ffb81a366637a5376233ff05e78fb4a0c
Tim Hoyt <tim.hoyt@twosixlabs.com>
2018-01-08 12:25:17 -0500

    Updated python peek/poke to use pythong mmap - Need to install pip + numpy

--- software/uzed.py
+++ software/uzed.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python
 
-from fpga_mmap import devmem
 import time
 import numpy as np
+from mmio import MMIO
 
 # Board/FPGA Constants
 AXI_GP0_ADDR_L = 0x40000000
@@ -12,16 +12,20 @@ TEST_BRAM_DEPTH = 1024
 
 # Generic peek/poke
 # Read/Write registers in the specified address space
-# Accepts list of integers to R/W buffers
-# Returns or prints list of integers from the buffer 
+# Lenth = 32-bit word length
+# Write value = numpy uint32 array, integer or list
 def peek(addr,length=1,echo=0):
 	# Add the HPM0 base address to requested address
 	addr = AXI_GP0_ADDR_L + addr
    	if AXI_GP0_ADDR_L <= addr <= AXI_GP0_ADDR_H: 
+   		byte_len = length << 2
    		# Map the memory buffer, print or return the register/buffer
-   		mem = devmem(addr,length)
-   		if echo == 1: print mem.read(length).hexdump(4)
-		else:  		 return mem.read(length)
+   		mem = MMIO(addr,byte_len)
+   		if echo == 1:
+   			mem.read(0x0,byte_len)
+   			print mem.hexdump()
+		else:  		 
+			return mem.read(0x0,byte_len)
    	else:
    		print ("ERROR: Address is outside FPGA memory space")
 
@@ -29,17 +33,30 @@ def poke(addr,value,mask=None):
 	# Add the HPM0 base address to requested address
 	base_addr = AXI_GP0_ADDR_L + addr
    	if AXI_GP0_ADDR_L <= base_addr <= AXI_GP0_ADDR_H: 
+   		# Accept int, list, or np.array types
+   		if type(value) is int:
+   			word_len = 1
+   			byte_len = 4
+   		else:
+   			word_len = len(value) 
+   			byte_len = len(value) << 2
    		# Map the memory buffer
-   		mem = devmem(base_addr,len(value))
+   		mem = MMIO(base_addr,byte_len)
    		# If a mask is needed, apply the mask to the entire buffer, or just write the buffer
-   		wrbuf = [value[i] for i in range(0,len(value))]
+   		# When word_len = 1 mem.read returns an integer, cast to a np.array and mask/value to a list
    		if mask is not None: 
-   			for i in range(0,len(value)):
-   				rdbuf = peek(addr)	
+   			if type(mask) is int: mask = [mask]
+   			if type(value) is int: value = [value]
+			rdbuf = np.uint32([mem.read(0x0,byte_len)])
+   			wrbuf = np.zeros(word_len,dtype=np.uint32)
+   			for i in range(0,word_len):	
    				wrbuf[i] = (rdbuf[0] & ~mask[i]) | (value[i] & mask[i])
    				addr += 4
-   		# Write the buffer
-   		mem.write(wrbuf)
+   			# Write the buffer
+   			mem.write(base_addr,wrbuf)
+   		else:
+   			# Write the value
+   			mem.write(base_addr,value)
    	else:
    		print ("ERROR: Address is outside FPGA memory space")
 
@@ -47,20 +64,19 @@ def poke(addr,value,mask=None):
 # Throughput = 4Kbyte / time elapsed
 def wr_benchmark(echo=True):
 	wr_value = [i for i in range(0,TEST_BRAM_DEPTH)]
+	wr_buf = np.uint32(wr_value)
 	t1 = time.time()
-	poke(TEST_BRAM_ADDR,wr_value)
+	poke(TEST_BRAM_ADDR,wr_buf)
 	t2 = time.time()
-	Bps = TEST_BRAM_DEPTH * 4 / (t2 - t1)
-	MBps = Bps / 1024
-	Mbps = MBps * 8
-	Gbps = Mbps / 1024
+	bps = (TEST_BRAM_DEPTH * 32) / (t2 - t1)
+	Mbps = bps / 1000000
+	MBps = Mbps / 8
 	if echo:
-		print "Time elapsed: %.3fs" % (t2 - t1)
-		print "MB/s: %.3f" % MBps
+		print "Time elapsed: %.5fs" % (t2 - t1)
 		print "Mb/s: %.3f" % Mbps
-		print "Gb/s: %.3f" % Gbps
+		print "MB/s: %.3f" % MBps
 	else:
-		return [MBps,Mbps,Gbps]
+		return [Mbps,MBps]
 
 # Read 4K, record the time elapsed and print throughput
 # Throughput = 4Kbyte / time elapsed
@@ -68,34 +84,30 @@ def rd_benchmark(echo=True):
 	t1 = time.time()
 	rd_value = peek(TEST_BRAM_ADDR,TEST_BRAM_DEPTH)
 	t2 = time.time()
-	Bps = TEST_BRAM_DEPTH * 4 / (t2 - t1)
-	MBps = Bps / 1024
-	Mbps = MBps * 8
-	Gbps = Mbps / 1024
+	bps = (TEST_BRAM_DEPTH * 32) / (t2 - t1)
+	Mbps = bps / 1000000
+	MBps = Mbps / 8
 	if echo:
-		print "Time elapsed: %.3fs" % (t2 - t1)
-		print "MB/s: %.3f" % MBps
+		print "Time elapsed: %.5fs" % (t2 - t1)
 		print "Mb/s: %.3f" % Mbps
-		print "Gb/s: %.3f" % Gbps
+		print "MB/s: %.3f" % MBps
 	else:
-		return [MBps,Mbps,Gbps]
+		return [Mbps,MBps]
 
 # Run a rd/wr benchmark of the FPGA fabbric
 # Read and write 4K blocks of memory for 'loop_cnt' iterations, print results
 def fpga_benchmark(loop_cnt):
 	print "Running WR Benchmark..."
-	wr_results = [0.0,0.0,0.0]
+	wr_results = [0.0,0.0]
 	for i in range(0,loop_cnt):
 		wr_results = np.add(wr_results,wr_benchmark(False))
 	print "Running RD Benchmark..."
-	rd_results = [0.0,0.0,0.0]
+	rd_results = [0.0,0.0]
 	for i in range(0,loop_cnt):
 		rd_results = np.add(rd_results,rd_benchmark(False))
 	print "WR Throughput:"
-	print "MB/s: %.3f" % (wr_results[0] / float(loop_cnt))
-	print "Mb/s: %.3f" % (wr_results[1] / float(loop_cnt))
-	print "Gb/s: %.3f" % (wr_results[2] / float(loop_cnt))
+	print "Mb/s: %.3f" % (wr_results[0] / float(loop_cnt))
+	print "MB/s: %.3f" % (wr_results[1] / float(loop_cnt))
 	print "RD Throughput:"
-	print "MB/s: %.3f" % (rd_results[0] / float(loop_cnt))
-	print "Mb/s: %.3f" % (rd_results[1] / float(loop_cnt))
-	print "Gb/s: %.3f" % (rd_results[2] / float(loop_cnt))
+	print "Mb/s: %.3f" % (rd_results[0] / float(loop_cnt))
+	print "MB/s: %.3f" % (rd_results[1] / float(loop_cnt))
